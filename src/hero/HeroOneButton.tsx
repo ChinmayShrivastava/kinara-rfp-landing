05771cba4e87bdb88c7986ac17dd80b8feb55c5e
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-03-17 12:48:05 -0400

    -minor

--- src/hero/HeroOneButton.tsx
+++ src/hero/HeroOneButton.tsx
@@ -8,10 +8,10 @@ type IHeroOneButtonProps = {
 
 const HeroOneButton = (props: IHeroOneButtonProps) => (
   <header className="text-center sm:text-left">
-    <h1 className="whitespace-pre-line text-[2.5em] leading-hero text-gray-900 sm:text-[4em]">
+    <h1 className="whitespace-pre-line text-[2em] text-gray-900 sm:text-[4em] sm:leading-hero">
       {props.title}
     </h1>
-    <div className="mb-16 mt-4 text-2xl">{props.description}</div>
+    <div className="mb-16 mt-4 text-xl sm:text-2xl">{props.description}</div>
     {props.button}
   </header>
 );
